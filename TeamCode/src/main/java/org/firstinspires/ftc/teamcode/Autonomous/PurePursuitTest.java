5a5e17719bc0c5009901cebaf3013fe532997715
Lucas <lrobindale@gmail.com>
2026-02-06 21:27:37 -0500

    Initial Pure Pursuit testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PurePursuitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PurePursuitTest.java
@@ -1,36 +1,44 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.PurePursuit.Path;
+import org.firstinspires.ftc.teamcode.PurePursuit.PurePursuiter;
+import org.firstinspires.ftc.teamcode.PurePursuit.Rotation2d;
+import org.firstinspires.ftc.teamcode.PurePursuit.Vector2d;
 import org.firstinspires.ftc.teamcode.PurePursuit.Waypoint;
+import org.firstinspires.ftc.teamcode.PurePursuit.WaypointGenerator;
+import org.firstinspires.ftc.teamcode.R;
 import org.firstinspires.ftc.teamcode.subsystems.Drivebase;
 
-@TeleOp
+@Autonomous
 public class PurePursuitTest extends LinearOpMode {
 
     @Override
     public void runOpMode() {
         Drivebase drive = new Drivebase(hardwareMap);
+        PurePursuiter pursuiter = new PurePursuiter(drive::getPosition, drive, telemetry, drive.getOdo());
+        drive.resetHeading(0);
 
-        Waypoint[] pathPoints = new Waypoint[] {
-                new Waypoint(0, 0, 0),
-                new Waypoint(50, 0, 0),
-                new Waypoint(100, 50,0)
-        };
+        double followRadius = 150;
 
-        Path path = new Path(pathPoints);
+        Path path = Path.getBuilder()
+                .addWaypoint(new Waypoint(0,0, followRadius, new Rotation2d(0), new Rotation2d( 0)))
+                .addWaypoint(new Waypoint(0, 10, followRadius, new Rotation2d(0), new Rotation2d(0)))
+                .build();
 
-        drive.setFollowPath(path);
+        pursuiter.setFollowPath(path);
+
 
         waitForStart();
 
-        while (opModeIsActive() && !drive.finishedFollowing()) {
-            drive.followPath();
+        while (opModeIsActive()) {
+            pursuiter.followPath();
             telemetry.addData("Robot Position", drive.getPositionTelemetry());
+            telemetry.update();
         }
-        drive.drive(0,0,0);
-        telemetry.update();
+
     }
 }
