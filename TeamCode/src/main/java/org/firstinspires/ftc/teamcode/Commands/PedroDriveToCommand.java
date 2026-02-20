83d0810544e7700f64efe76915be7f509e96fef4
Lucas <lrobindale@gmail.com>
2026-02-19 21:24:59 -0500

    Implemented roughly tuned pedropathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/PedroDriveToCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/PedroDriveToCommand.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Commands;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Autonomous.Location;
@@ -13,15 +14,19 @@ public class PedroDriveToCommand extends Command {
     Telemetry telemetry;
     private final Follower follower;
     private final PathChain path;
+    double timeout;
+    ElapsedTime time = new ElapsedTime();
 
-    public PedroDriveToCommand(Follower follower, PathChain path) {
+    public PedroDriveToCommand(Follower follower, PathChain path, double timeout) {
         this.follower = follower;
         this.path = path;
+        this.timeout = timeout;
     }
 
     @Override
     public void initialize() {
         follower.followPath(path);
+        time.reset();
     }
 
     @Override
@@ -31,6 +36,6 @@ public class PedroDriveToCommand extends Command {
 
     @Override
     public boolean isFinished() {
-        return !follower.isBusy();
+        return !follower.isBusy() || time.milliseconds() >= timeout;
     }
 }
